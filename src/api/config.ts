c0bd5a234801086771619f725a0bdaa2678e5410
Jolaoluwa Olusanya <olusanyajolaoluwa@gmail.com>
2026-02-22 20:02:59 +0000

    updates

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 const url = {
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
-  hostedUrl: import.meta.env.VITE_HOSTED_URL || "",
-  mobileUrl: import.meta.env.VITE_MOBILE_URL || "",
+  hostedUrl: import.meta.env.VITE_HOSTED_URL || "https://cribxpert-backend.onrender.com",
+  mobileUrl: import.meta.env.VITE_MOBILE_URL || "http://192.168.0.145:5000",
 };
 
 export default url;
